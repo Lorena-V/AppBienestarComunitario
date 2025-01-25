bec7eb1a6de6f196f75e76b91b66ad89a42d1989
Lorena Gonzalez <gonzalezm.lorena2@gmail.com>
2025-01-25 17:08:19 -0300

    Incorporación de modal funcionando

--- src/app/c/aviso-list/aviso-list.component.ts
+++ src/app/c/aviso-list/aviso-list.component.ts
@@ -2,8 +2,8 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { CommonModule } from '@angular/common'; //para usar ngForm
 import { Aviso } from 'src/app/entidad/Aviso';
 import { IonButton, IonIcon, IonThumbnail, IonImg, IonFab, IonFabButton, 
-  IonHeader, IonContent ,IonToolbar, IonTitle, IonList, IonItem, IonLabel,
- } from '@ionic/angular/standalone';
+  IonHeader, IonContent ,IonToolbar, IonTitle, IonList, IonItem, IonLabel, 
+  IonButtons, IonModal } from '@ionic/angular/standalone';
 
 import { addIcons } from 'ionicons';
 import { trashOutline, add } from 'ionicons/icons';
@@ -14,12 +14,14 @@ import { trashOutline, add } from 'ionicons/icons';
   styleUrls: ['./aviso-list.component.scss'],
   imports: [CommonModule, IonIcon, IonButton, IonList, IonItem, IonLabel, 
     IonThumbnail, IonImg, IonFab, IonFabButton, IonHeader, IonToolbar,
-    IonTitle],
+    IonTitle, IonButtons, IonModal ],
   standalone: true,
 })
 
 // clase que recive datos del aviso
 export class AvisoListComponent  implements OnInit {
+
+  isModalPriceOpen: boolean = false
 
   @Input() avisos: Aviso[] = [] // arreglo de avisos que empieza con un array vacío
 
@@ -28,18 +30,22 @@ export class AvisoListComponent  implements OnInit {
 
   constructor() {
       addIcons({trashOutline,add});
-    
    }
 
   ngOnInit() {}
 
   clickEliminar(a:Aviso) {
     this.onDelete.emit(a);
+    this.setModalPriceOpen(true)
   }
 
   clickAgregar() {
     this.onAddAviso.emit();
     console.log("ir al form" )
+  }
+
+  setModalPriceOpen(abierto:boolean) {
+    this.isModalPriceOpen = abierto
   }
 
 }
